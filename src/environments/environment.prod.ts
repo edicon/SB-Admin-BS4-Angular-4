8bc39dd01e19a75fa09ce0107361e8807093810a
EDICON <hslee.edicon@gmail.com>
2017-06-09 12:03:08 +0900

    Create environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
 export const environment = {
   production: true,
     firebase: {
-      apiKey: "AIzaSyBFXdf8QoQEi83DpYZJRINMQNKTWoGlNXI",
-      authDomain: "mdic.firebaseapp.com",
-      databaseURL: "https://mdic.firebaseio.com",
-      projectId: "firebase-mdic",
-      storageBucket: "firebase-mdic.appspot.com",
-      messagingSenderId: "683153513136"
   }
 };
